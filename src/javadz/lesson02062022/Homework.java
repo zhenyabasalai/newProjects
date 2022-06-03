3b6331c5630a29c7908f4ab30534ddd38f848f2a
Zhenya <zh.basalay@gmail.com>
2022-06-04 00:34:50 +0300

    Hello World

---
+++ src/javadz/lesson02062022/Homework.java
@@ -0,0 +1,13 @@
+package javadz.lesson02062022;
+
+public class Homework {
+
+       public static void main(String[] args)  {
+
+           System.out.println("Hello World");
+
+        
+
+    }
+
+}
