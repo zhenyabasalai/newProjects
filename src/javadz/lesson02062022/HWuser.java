f8b0e438cde699a24ecdce4a32c46ac30837b5f6
Zhenya <zh.basalay@gmail.com>
2022-06-06 20:29:13 +0300

    доп ДЗ

---
+++ src/javadz/lesson02062022/HWuser.java
@@ -0,0 +1,18 @@
+package javadz.lesson02062022;
+
+import java.util.Scanner;
+
+public class HWuser {
+    public static void main(String[] args)  {
+
+      String user;
+      Scanner input=new Scanner(System.in);
+      System.out.print("Введите <user> : ");
+      user=input.nextLine();
+
+      System.out.println("Hello " + user);
+
+
+  }
+
+}
