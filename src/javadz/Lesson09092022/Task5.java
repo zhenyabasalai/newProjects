20ec1696169905beaf92b5a037c3555972f76788
Zhenya <zh.basalay@gmail.com>
2022-06-10 16:30:31 +0300

    Create a second pattern of even elements of the first pattern

---
+++ src/javadz/Lesson09092022/Task5.java
@@ -0,0 +1,43 @@
+package javadz.Lesson09092022;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Task5 {
+    public static void main(String[] args) {
+        int[] array;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input a number : ");
+        int i = 0;
+        while (i <= 5 || i > 10) {
+            if (input.hasNextInt()) {
+                i = input.nextInt();
+                if (i <= 5 || i > 10) {
+                    System.out.println("PLease input corect number: ");
+                }
+            } else {
+                System.out.println("You entered a not number, repeat : ");
+                input.next();
+            }
+        }
+        array = new int[i];
+        int evenNums = 0;
+
+        for (int a = 0; a < i; a++) {
+            array[a] = ((int) (Math.random() * i));
+            if (array[a] % 2 == 0) {
+                evenNums++;
+            }
+        }
+        System.out.println(Arrays.toString(array));
+        int[] array2 = new int[evenNums];
+        int index = 0;
+        for (int a = 0; a < i; a++) {
+            if (array[a] % 2 == 0) {
+                array2[index] = array[a];
+                index++;
+            }
+        }
+        System.out.println(Arrays.toString(array2));
+    }
+}
