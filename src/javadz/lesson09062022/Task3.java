bbd14d0237a08e2e70abaafe9555f7d7405fb76d
Zhenya <zh.basalay@gmail.com>
2022-06-14 01:49:31 +0300

    Equal to, greater than or less than the average of two arrays

---
+++ src/javadz/lesson09062022/Task3.java
@@ -0,0 +1,28 @@
+package javadz.lesson09062022;
+
+import java.util.Arrays;
+
+public class Task3 {
+    public static void main(String[] args) {
+        int[] arrays1 = {4, 8, 7, 5, 2};
+        System.out.println(Arrays.toString(arrays1));
+        int[] arrays2 = {3, 8, 4, 5, 4};
+        System.out.println(Arrays.toString(arrays2));
+        double sum = 0;
+        for (int x : arrays1) {
+            sum += x;
+        }
+        double sum1 = 0;
+        for (int x : arrays2) {
+            sum1 += x;
+        }
+        if (sum1 > sum) {
+            System.out.println("Еhe average value of the second array is greater than the first");
+        } else if (sum > sum1) {
+            System.out.println("The average value of the first array is greater than the second");
+
+        } else {
+            System.out.println("The average values of the first and second are");
+        }
+    }
+}
