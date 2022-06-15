724d54bb0f12cba7d0ec02a2a19ccb9496597afe
Zhenya <zh.basalay@gmail.com>
2022-06-15 20:52:27 +0300

    Сreate a three-dimensional array of integers and increase by the number entered

--- src/javadz/lesson14062022/Task1.java
+++ src/javadz/lesson14062022/Task1.java
@@ -1,19 +1,24 @@
 package javadz.lesson14062022;
 
+import java.util.Arrays;
+import java.util.Scanner;
 public class Task1 {
-
     public static void main(String[] args) {
 
-        String[][] chess = new String[8][8];
-        for (int i = 0; i < chess.length; i++) {
-            for (int j = 0; j < chess[0].length; j++) {
-                if ((i + j) % 2 == 0)
-                    chess[i][j] = "W";
-                else chess[i][j] = "B";
-                System.out.print(" " + chess[i][j] + " ");
+        int[][][] array = {{{4, 7, 8, 4}, {7, 2, 1}}, {{5, 6, 3}}};
+        System.out.print("Input namber: ");
+        Scanner namb = new Scanner(System.in);
+        int a = namb.nextInt();
+        System.out.println(Arrays.deepToString(array));
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                for (int k = 0; k < array[i][j].length; k++) {
+
+                    array[i][j][k] += a;
+                }
             }
-            System.out.println();
         }
+        System.out.println(Arrays.deepToString(array));
     }
 }
 
