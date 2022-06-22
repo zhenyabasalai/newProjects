22b3c506032a39fe7b713b4e6a1fdd2b8eea9eae
Zhenya <zh.basalay@gmail.com>
2022-06-22 15:32:40 +0300

    Task2, create 3 class ... create interface with method

---
+++ src/javadz/lesson21062022/task2/TitleJob.java
@@ -0,0 +1,5 @@
+package javadz.lesson21062022.task2;
+
+public interface TitleJob {
+    String title();
+}
---
+++ src/javadz/lesson21062022/task2/Worker.java
@@ -0,0 +1,11 @@
+package javadz.lesson21062022.task2;
+
+public class Worker implements TitleJob {
+    String title = "Worker";
+    public String title() {
+        System.out.println(title);
+        return null;
+    }
+}
+
+
