3e010e833c977141cbde46c8080c59ce74f87f13
Zhenya <zh.basalay@gmail.com>
2022-07-12 14:24:39 +0300

    Task1, reading txt doc from file

---
+++ src/javadz/lesson07072022/Task1.java
@@ -0,0 +1,21 @@
+package javadz.lesson07072022;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) throws FileNotFoundException {
+        String path = "C://Users//User//OneDrive//Рабочий стол//Task1.txt";
+
+        File file = new File(path);
+
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNextLine()) {
+            System.out.println(scanner.nextLine());
+        }
+        scanner.close();
+
+    }
+
+}
