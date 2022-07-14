7c617333a40910441dc5e6364269d22c36cb2446
Zhenya <zh.basalay@gmail.com>
2022-07-14 07:07:47 +0300

    Task1,create class Student ......

---
+++ src/javadz/lesson12072022/Task1/Student.java
@@ -0,0 +1,34 @@
+package javadz.lesson12072022.Task1;
+
+public class Student {
+
+    String name;
+    float mark;
+
+    public Student() {
+    }
+
+    public Student(String name, float mark) {
+        this.name = name;
+        this.mark = mark;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getMark() {
+        return mark;
+    }
+
+    public void setMark(float mark) {
+        this.mark = mark;
+    }
+}
+
+
